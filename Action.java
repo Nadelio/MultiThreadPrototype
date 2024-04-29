696c288a4f3cf6e609386bc5f24333f551a1774e
Nadelio <carefulsniffle@gmail.com>
2024-04-29 15:05:41 -0500

    Added Action class and wrote a way to run an Action's effectProcess() NEED TO MAKE IT PROCEDURAL

---
+++ Action.java
@@ -0,0 +1,29 @@
+public class Action
+{
+    private boolean hasEffect;
+    private int damage;
+    private String type;
+    private String name;
+    private boolean isHarmful;
+
+    public Action(){}
+
+    public Action(boolean hasEffect, int damage, String type, String name, boolean isHarmful)
+    {
+        this.hasEffect = hasEffect;
+        this.damage = damage;
+        this.type = type;
+        this.name = name;
+        this.isHarmful = isHarmful;
+    }
+
+    public boolean getHasEffect(){return hasEffect;}
+    public int getActionDamage(){return damage;}
+    public String getType(){return type;}
+    public String getName(){return name;}
+    public boolean getIsHarmful(){return isHarmful;}
+
+    public void effectProcess(){}
+
+    public String toString(){return getName();}
+}
--- ThreadedMain.java
+++ ThreadedMain.java
@@ -10,11 +10,25 @@ public class ThreadedMain extends Thread
         "_( - .-)_"        
     };
 
+    public static boolean turnUpdate;
+
     private static ThreadedMain main = new ThreadedMain();
     public static void main(String[] args)
     {
+        
+
         main.start();
-        runAnim();
+        for(int i = 3; i > -1; i--)
+        {
+            System.out.println("Turn Start!");
+            turnUpdate = true;
+            try {Thread.sleep(25);}catch(Exception e){}
+            System.out.println("Turn End!");
+            turnUpdate = false;
+
+            try {Thread.sleep(1000);}catch(Exception e){}
+        }
+        //runAnim();
     }
 
     public static void runAnim()
@@ -29,7 +43,37 @@ public class ThreadedMain extends Thread
     @Override
     public void run()
     {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
+        //System.out.print("\033[H\033[2J");
+        //System.out.flush();
+        Action action1 = new Action(true, 0, "NONE", "TestAction1", false){
+            @Override
+            public void effectProcess(){
+                int burnDuration = 3;
+                while(burnDuration > 0){
+                    if(turnUpdate){
+                        burnDuration--;
+                        System.out.println(this.getName() + " Duration: " + burnDuration);
+                        try {Thread.sleep(50);} catch(InterruptedException e){}
+                    }
+                }
+            }
+        };
+
+        Action action2 = new Action(true, 0, "NONE", "TestAction2", false){
+            @Override
+            public void effectProcess(){
+                int burnDuration = 2;
+                while(burnDuration > 0){
+                    if(turnUpdate){
+                        burnDuration--;
+                        System.out.println(this.getName() + " Duration: " + burnDuration);
+                        try {Thread.sleep(50);} catch(InterruptedException e){}
+                    }
+                }
+            }
+        };
+
+        action1.effectProcess();
+        action2.effectProcess();
+    } //! need to develop some way to procedurally generate Threads or flip between action1.effectProcess() and action2.effectProcess()
 }
